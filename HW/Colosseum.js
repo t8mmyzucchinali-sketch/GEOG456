59ff1fc05484d6efa3371d343c05a776ff97bf2a
t8mmyzucchinali-sketch <t8mmy.zucchinali@gmail.com>
2026-01-22 15:44:45 -0500

    Create Colosseum.js

---
+++ HW/Colosseum.js
@@ -0,0 +1,7 @@
+var Colosseum = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "id": 1, "descrip": "Colosseum" }, "geometry": { "type": "Point", "coordinates": [12.4922, 41.8902] } }
+    ]
+    }
